8980b1947ab751b05181ec20512bb6df4b195745
BrunoPignatar <zemane1312@gmail.com>
2023-08-21 15:36:12 -0300

    verificando o frame certo

--- teste bola fifa/teste bola fifa/MainPage.xaml.cs
+++ teste bola fifa/teste bola fifa/MainPage.xaml.cs
@@ -15,15 +15,12 @@ namespace teste_bola_fifa
             InitializeComponent();
         }
 
-        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
-        {
-            Image image = (sender as Element)?.Parent as Image;
-            image.Opacity = 0.4;
-            e.Data.Properties.Add("Demo", image);
-        }
+        bool certo;
+        
 
         private async void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
         {
+            certo = true; 
             var image = e.Data.Properties["Demo"] as Image;
             var frame = (sender as Element)?. Parent as Frame;
             frame.Content = image;
@@ -31,6 +28,42 @@ namespace teste_bola_fifa
                 await image.ScaleTo(1.4, 400);
                 await image.FadeTo(1, 600);
             });
+            verificador();
+        }
+
+        private void DragGestureRecognizer_DragStarting_1(object sender, DragStartingEventArgs e)
+        {
+            Image image = (sender as Element)?.Parent as Image;
+            image.Opacity = 0.4;
+            e.Data.Properties.Add("Demo", image);
+        }
+
+        private void DropGestureRecognizer_Drop_1(object sender, DropEventArgs e)
+        {
+            certo = false;
+            var image = e.Data.Properties["Demo"] as Image;
+            var frame = (sender as Element)?.Parent as Frame;
+            frame.Content = image;
+            Task.Run(async () => {
+                await image.ScaleTo(1.4, 400);
+                await image.FadeTo(1, 600);
+               
+            });
+            verificador();
+           
+        }
+
+        public async void verificador() 
+        {
+            if (certo == false)
+            {
+                await DisplayAlert("Que Pena", "Você errou o quadrado", "ok");
+            }
+            else
+            {
+                await DisplayAlert("parabéns", "Você acertou o quadrado", "ok" );
+            }
+
         }
     }
 }
